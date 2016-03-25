fd331e622b7a3c8a1ff5c1b0e041392f84066418
charles <liucch3@gmail.com>
2016-03-25 13:10:14 +0800

    d
    dd

--- sparkDataCube.py
+++ sparkDataCube.py
@@ -59,6 +59,8 @@ def navie_mapper(line): # line: line record
 	
 # -- navie_mapper --
 
+
+
 # -- batch_mapper --
 def batch_mapper(line):
 	data = line.strip().split()
@@ -75,7 +77,49 @@ def batch_mapper(line):
 		batch_id += 1
 # -- batch_mapper --
 
+"""
 
+
+lgorithm 2. Overall MR-Cube Algorithm MR-CUBE(Cube Lattice C, Data set D, Measure M)
+1 Dsample 1⁄4 SAMPLEðDÞ
+2 RegionSizes R 1⁄4 ESTIMATE-MAPREDUCE
+ðDsample ; CÞ
+3 Ca 1⁄4 ANNOTATEðR; CÞ # value part. & batching 4 while (D)
+5 do R
+6 D
+7 Ca
+8 Result
+9 return Result
+R [ MR-CUBE-MAPREDUCEðCa ; M; DÞ
+D’ # retry failed groups D’ from MR-Cube-Reduce INCREASE-PARTITIONINGðCa Þ
+MERGE(R)# post-aggregate value partitions
+Algorithm 3. MR-Cube Phase 1: Annotation MapReduce ESTIMATE-Map(e)
+1 #eisatupleinthedata
+2 let C be the Cube Lattice;
+3 foreachci inC
+4 do EMITðci;ciðeÞ ) 1Þ # the group is the secondary key
+ESTIMATE-REDUCE/COMBINE(hr; gi; fe1; e2; . . .g) 1 # hr; gi are the primary/secondary keys
+2 MaxSizeS fg
+3 for each r,g
+4 do S[r] MAX(S[r],jgj)
+5 # jgj is the number of tuples fei;...;ejg 2 g 6 return S
+
+Algorithm PipeSort
+
+function PS_MAP(e):
+	# e is a tuple in the dataset
+	C = getPipeline() // C is a list of pipeline, pipeline is a list of group with the same 前缀
+	batch_id = 0
+	for all R in C do:
+		re = R(e) // R(e): extra the 属性 of longest group in R from e
+		do EMIT(batch_id, re=>uid) // uid: record id, the uid of tuple e
+		batch_id += 1
+
+function PS_REDUCE(batch_id, [kv1, kv2, kv3....]):
+	
+
+
+"""
 
 # -- top_down --
 def top_down(data):
@@ -131,6 +175,7 @@ def main_func(sc, rdd):
 	#tmp = rdd.flatMap(batch_mapper).collect()
 
 	tmp = rdd.flatMap(batch_mapper).groupByKey().flatMapValues(top_down).collect()
+	#tmp = rdd.flatMap(batch_mapper).groupByKey().flatMapValues(top_down).collect()
 	print tmp
 
 # -- navie_reducer --
@@ -140,6 +185,7 @@ if __name__ == "__main__":
 	rdd = sc.textFile("file:///Users/liucancheng/Documents/GitHub/sparkDataCube/test.txt")
 	main_func(sc, rdd)
 	print "888888888888888888888888888888888888888888888888888888888888888888888888888888888888888"
+
 
 
 
